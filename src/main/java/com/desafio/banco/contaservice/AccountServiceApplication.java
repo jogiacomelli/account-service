15b404015eb767e0cdae8cb71259b05c977a055a
jogiacomelli <jogiacomelli.cc@gmail.com>
2020-05-31 19:12:50 -0300

    MessageListener receive message from user-service and create account to the given user.

--- src/main/java/com/desafio/banco/contaservice/AccountServiceApplication.java
+++ src/main/java/com/desafio/banco/contaservice/AccountServiceApplication.java
@@ -2,10 +2,12 @@ package com.desafio.banco.contaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@EnableEurekaClient
 @PropertySources({
 		@PropertySource("classpath:account-limits.properties"),
 		@PropertySource("classpath:creditcard-limits.properties"),
--- src/main/java/com/desafio/banco/contaservice/messages/MessageListener.java
+++ src/main/java/com/desafio/banco/contaservice/messages/MessageListener.java
@@ -1,4 +1,21 @@
 package com.desafio.banco.contaservice.messages;
 
+import com.desafio.banco.contaservice.dto.UserDTO;
+import com.desafio.banco.contaservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
 public class MessageListener {
+
+    @Autowired
+    private AccountService accountService;
+
+    @StreamListener(Sink.INPUT)
+    public void readMessage(UserDTO user) {
+        accountService.createAccountToUser(user);
+    }
+
 }
